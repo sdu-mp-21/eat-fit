5f3bbbc3d408a4ef7fa17265cf77cfb71acac848
RavilAmageldiuly <79452777+RavilAmageldiuly@users.noreply.github.com>
2021-10-18 21:03:26 +0600

    Каркас сделан

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'lockscreen.dart';
+import 'NavigationDrawerWidget.dart';
+import 'TrainingPage.dart';
+import 'MenuPage.dart';
+import 'MainPage.dart';
+
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => const Home(),
+    '/lock': (context) => const LockScreen(),
+    '/training': (context) => const TrainingPage(),
+    '/menu': (context) => const MenuPage(),
+  },
+  debugShowCheckedModeBanner: false,
 ));
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,8 +26,60 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  List<Widget> pages = [
+    TrainingPage(),
+    MainPage(),
+    MenuPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: SideNavigationBar(),
+      appBar: AppBar(
+        title: Text('Eat and Fit'),
+        backgroundColor: Color.fromRGBO(39, 16, 51, 1),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: pages.elementAt(_currentIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        iconSize: 30,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_balance,
+            ),
+            title: Text('Training'),
+            backgroundColor: Colors.purple[500],
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.attach_money,
+            ),
+            title: Text('Main'),
+            backgroundColor: Colors.teal,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_balance_wallet_rounded,
+            ),
+            title: Text('Menu'),
+            backgroundColor: Colors.deepOrange,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
+
