272bf525db217b70efa372dcdac9e14bb2b68b87
190103494 <63901134+190103494@users.noreply.github.com>
2021-11-02 13:02:17 +0600

    Update favorites_page.dart

--- lib/views/favorites_page.dart
+++ lib/views/favorites_page.dart
@@ -11,7 +11,56 @@ class _FavoritesPageState extends State<FavoritesPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Страница Избранных блюд'),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            setFavoriteMenuItem('oladushki', 'Творожные оладушки', 20, Colors.black, '10', 16),
+            setFavoriteMenuItem('pirog', 'Картофельный пирог с яйцом', 20, Colors.black, '40', 16),
+            setFavoriteMenuItem('bulghur', 'Булгур с курочкой', 20, Colors.black, '55', 16),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget setText(String text, double fontSize, Color fontColor) {
+    return Text(
+      '$text',
+      style: TextStyle(
+        fontSize: fontSize,
+        color: fontColor,
+      ),
+    );
+  }
+  
+  Widget setFavoriteMenuItem(String imageName, String mealName, double nameSize, Color textColor, String time, double timeSize) {
+    return Row(
+      children: [
+        Image.asset(
+          'assets/$imageName.png',
+          width: 80,
+          height: 80,
+        ),
+        SizedBox(width: 15,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            setText(mealName, nameSize, textColor),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Icon(
+                  Icons.timer_rounded,
+                  size: timeSize,
+                ),
+                setText('$time мин', timeSize, textColor),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
