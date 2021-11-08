a1278cdec718530deb2aebcb4f377f2a0d438995
dxlas <75566554+dxlas@users.noreply.github.com>
2021-11-09 00:13:37 +0600

    Create exercise_page.dart

---
+++ lib/views/exercise_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ExercisePage extends StatefulWidget {
+  const ExercisePage({Key? key}) : super(key: key);
+
+  @override
+  _ExercisePageState createState() => _ExercisePageState();
+}
+
+class _ExercisePageState extends State<ExercisePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _buildText('Exercise Name', 22, Colors.white),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildText(String text, double size, Color color) {
+    return Text(
+      '$text',
+      style: TextStyle(
+        fontSize: size,
+        color: color,
+      ),
+    );
+  }
+}
