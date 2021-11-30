9da4dae453c8ae30e233e531a996d7984fb4f00c
RavilAmageldiuly <79452777+RavilAmageldiuly@users.noreply.github.com>
2021-11-30 16:16:26 +0600

    Update about_page.dart

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -10,54 +10,180 @@ class AboutPage extends StatelessWidget {
         title: const Text(
           'О приложении',
           style: TextStyle(
-            fontSize: 24,
+            fontSize: 22,
             color: Colors.white,
           ),
         ),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: const [
-            Text(
-              'Фитнес приложение Eat&Fit - ваш помощник в достижении тела вашей мечты',
-              style: TextStyle(
-                fontSize: 20,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/ic_launcher.png',
+                      width: 140,
+                      height: 140,
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      'Фитнес приложение Eat&Fit - ваш помощник в достижении тела вашей мечты',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 15,),
-            Text(
-              '''
-* Актуальные данные о погоде вашего города
-* Узнайте калорийность упоребляемых вами продуктов
-* Составьте свой рацион здорового питания
-* Занимайтесь спортом без спортивного инвентаря и без привязки к одному месту
-* Будьте в курсе о количестве сожженых и потребленных калории
-              ''',
-              style: TextStyle(
-                fontSize: 18,
+              SizedBox(height: 45,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Актуальные данные о\nпогоде вашего города',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/about_page_pack/weather-app.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            Text(
-              'В будущих обновлениях ожидаются:',
-              style: TextStyle(
-                fontSize: 20,
+              SizedBox(height: 35,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/about_page_pack/calories-calculator.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Text(
+                      'Узнайте калорийность\nупоребляемых вами продуктов',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            Text(
-              '''
-* Шагомер
-* Вожможность добавлять и удалять рецепты
-* To do list
-              ''',
-              style: TextStyle(
-                fontSize: 18,
+              SizedBox(height: 35,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Составьте свой рацион\nздорового питания',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/about_page_pack/diet.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 35,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/about_page_pack/no-sport.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    Text(
+                      'Занимайтесь спортом без\nспортивного инвентаря и без\nпривязки к оределенному\nместу',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 35,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Будьте в курсе о\nколичестве сожженых и\nпотребленных калории',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/about_page_pack/calories.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40,),
+              Text(
+                'В будущих обновлениях ожидаются:',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/about_page_pack/pedometer.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    SizedBox(width: 20,),
+                    Text(
+                      'Шагомер',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 12,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Список дел',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Image.asset(
+                      'assets/about_page_pack/to-do-list.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
